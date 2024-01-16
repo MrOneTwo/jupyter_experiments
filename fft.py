cbf634b7db6d73bf1f70ca5031d25877e547d6e0
Michal Ciesielski <ciesielskimm@gmail.com>
2024-01-16 22:26:05 +0100

    Manually rewriting to Marimo notebook

--- fft.py
+++ fft.py
@@ -5,80 +5,67 @@ app = marimo.App()
 
 
 @app.cell
-def _():
-    import numpy as np
+def __():
+    import marimo as mo
     from pathlib import Path
     import struct
 
-    data_unpacked = np.asarray([d[0] for d in struct.iter_unpack("<H", Path("samples.bin").read_bytes())])
-    return Path, data_unpacked, np, struct
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    data_unpacked = np.asarray(
+        [d[0] for d in struct.iter_unpack("<H", Path("samples.bin").read_bytes())]
+    )
+    return Path, data_unpacked, mo, np, plt, struct
 
 
 @app.cell
-def _(mo):
-    mo.md(
-        r"""
-        Loaded the 2 byte sample data.
-
-        Lets proceed to plotting the scaled data.
-        """
-    )
+def __(mo):
+    mo.md("Loaded the 2 byte sample data.")
     return
 
 
 @app.cell
-def _(data_unpacked, np):
-    import matplotlib.pyplot as plt
-
-    data_float = []
-    to_float = lambda x: x / 8192.0
-    data_float = to_float(data_unpacked)
+def __(data_unpacked, np, plt):
+    data_float = list()
+    normalize_data = lambda x: x/ 8192.0
+    data_float = normalize_data(data_unpacked)
 
     FREQUENCY = 32 * 1000
     BYTES_PER_SAMPLE = 2
 
-    data_to_plot = data_float
+    _data_to_plot = data_float
+    dt = 1.0 /FREQUENCY
+    _t = np.arange(0, len(_data_to_plot), 1)
 
-    dt = 1.0 / FREQUENCY
-    t = np.arange(0, len(data_to_plot), 1)
+    _fig, _axs = plt.subplots(len((_data_to_plot, [])))
 
-    fig, axs = plt.subplots(len((data_to_plot, [])))
+    for _i, _data in enumerate((_data_to_plot, )):
+        _axs[_i].set_ylim([_data.mean() - 0.05, _data.mean() + 0.05])
+        _axs[_i].plot(_t, _data, linewidth=0.1)
+        _axs[_i].set(xlabel='sample', ylabel='val', title='Soundwave plot')
+        _axs[_i].grid(color='k', alpha=0.2, linestyle='-.', linewidth=0.5)
 
-    for i, data in enumerate((data_to_plot, )):
-        axs[i].set_ylim([data.mean() - 0.05, data.mean() + 0.05])
-        axs[i].plot(t, data, linewidth=0.1)
-        axs[i].set(xlabel='sample', ylabel='val', title='Soundwave plot')
-        axs[i].grid(color='k', alpha=0.2, linestyle='-.', linewidth=0.5)
-    return (
-        BYTES_PER_SAMPLE,
-        FREQUENCY,
-        axs,
-        data,
-        data_float,
-        data_to_plot,
-        dt,
-        fig,
-        i,
-        plt,
-        t,
-        to_float,
-    )
+    _fig
+    return BYTES_PER_SAMPLE, FREQUENCY, data_float, dt, normalize_data
 
 
 @app.cell
-def _(mo):
-    mo.md(
-        r"""
-        Lets test some simple FFT case.
-        """
-    )
+def __(mo):
+    mo.md(r"""Here is the simplified DFT
+
+    \[
+    X_k = \sum_{n=0}^{N-1} x_n \sin(2\pi\frac{k}{N}n)
+    \]
+    """)
     return
 
 
 @app.cell
-def _():
-    from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
+def __(np, plt):
     from dataclasses import dataclass
+    from matplotlib.ticker import MultipleLocator, AutoMinorLocator
+
 
     @dataclass
     class Waveform:
@@ -92,47 +79,47 @@ def _():
             t = np.arange(0, length, length / self.resolution)
             return t, self.amplitude * np.sin(t)
 
-    t, harmonic01 = Waveform(cycles=4, amplitude=1).get_wave()
+
+    _t, harmonic01 = Waveform(cycles=4, amplitude=1).get_wave()
     _, harmonic02 = Waveform(cycles=12, amplitude=2).get_wave()
-    #_, waveform03 = Waveform(cycles=5).get_wave()
+    # _, waveform03 = Waveform(cycles=5).get_wave()
+
     waveform = harmonic01 + harmonic02
 
-    harmonics = np.zeros(len(t))
-    N = len(waveform)
-    for k, j in enumerate(range(N)):    
+    harmonics = np.zeros(len(_t))
+    _N = len(waveform)
+
+    for k, j in enumerate(range(_N)):
         potential_harmonic = 0
         for i, x in enumerate(waveform):
             n = i
-            potential_harmonic += x * np.sin(np.pi * 2 * (k/N) * n)
-
+            potential_harmonic += x * np.sin(np.pi * 2 * (k / _N) * n)
         print(f"harmonic {j} is {potential_harmonic}")
         # Dividing by N means normalizing the result.
-        harmonics[j] = potential_harmonic / N
+        harmonics[j] = potential_harmonic / _N
 
     data_to_plot = (harmonic01, harmonic02, waveform, harmonics)
-    fig, axs = plt.subplots(len(data_to_plot), figsize=(14, 14))
+    _fig, _axs = plt.subplots(len(data_to_plot), figsize=(14, 14))
 
     for i, data in enumerate(data_to_plot[:-1]):
-        axs[i].set_ylim([data.min() - 0.4, data.max() + 0.4])
-        axs[i].plot(t, data, linewidth=0.5)
-        #axs[i].set(xlabel='sample', ylabel='val', title='Soundwave plot')
-        axs[i].grid(color='k', alpha=0.2, linestyle='-.', linewidth=0.5)
+        _axs[i].set_ylim([data.min() - 0.4, data.max() + 0.4])
+        _axs[i].plot(_t, data, linewidth=0.5)
+        # _axs[i].set(xlabel='sample', ylabel='val', title='Soundwave plot')
+        _axs[i].grid(color="k", alpha=0.2, linestyle="-.", linewidth=0.5)
 
-    axs[-1].bar(np.arange(len(harmonics)), harmonics)
-    axs[-1].grid(color='k', alpha=0.2, linestyle='-.', linewidth=0.5)
-    axs[-1].xaxis.set_major_locator(MultipleLocator(10))
-    axs[-1].xaxis.set_minor_locator(MultipleLocator(5))
-        
+    _axs[-1].bar(np.arange(len(harmonics)), harmonics)
+    _axs[-1].grid(color="k", alpha=0.2, linestyle="-.", linewidth=0.5)
+    _axs[-1].xaxis.set_major_locator(MultipleLocator(10))
+    _axs[-1].xaxis.set_minor_locator(MultipleLocator(5))
+
+    _fig
     return (
         AutoMinorLocator,
         MultipleLocator,
-        N,
         Waveform,
-        axs,
         data,
         data_to_plot,
         dataclass,
-        fig,
         harmonic01,
         harmonic02,
         harmonics,
@@ -140,26 +127,11 @@ def _():
         j,
         k,
         n,
-        np,
-        plt,
         potential_harmonic,
-        t,
         waveform,
         x,
     )
 
 
-@app.cell
-def _():
-    return
-
-
-@app.cell
-def _():
-    import marimo as mo
-    return mo,
-
-
 if __name__ == "__main__":
     app.run()
-
