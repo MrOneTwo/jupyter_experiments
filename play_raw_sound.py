5fbea33c113b9b4b23edca3b5f83c42ad197bbee
Michal Ciesielski <ciesielskimm@gmail.com>
2024-04-22 21:25:11 +0200

    Improve play_raw_sound.py

--- play_raw_sound.py
+++ play_raw_sound.py
@@ -6,22 +6,31 @@ from config import *
 
 INPUT_FILE = SAMPLES_FILE
 
-data_unpacked = np.asarray([d[0] for d in struct.iter_unpack("<H", INPUT_FILE.read_bytes())])
 
-data_float = []
+def play_pcm(pcm_file: Path, frequency: int, sample_size: int):
+    data_unpacked = np.asarray(
+        [d[0] for d in struct.iter_unpack("<h", pcm_file.read_bytes())]
+    )
+    samples_count = len(data_unpacked)
 
-to_float = lambda x: x / 8192.0
-data_float = to_float(data_unpacked)
+    normalize_factor = np.iinfo(np.int16).max
+    to_float = lambda x: x / normalize_factor
+    data_float = to_float(data_unpacked)
 
-samples_count = len(data_unpacked)
+    seconds = samples_count / frequency
 
-seconds = samples_count / FREQUENCY
+    print(f"samples: {samples_count}, (min {data_unpacked.min()},  max {data_unpacked.max()})")
+    print(f"float: (min {data_float.min()}, max {data_float.max()}")
+    print(f"length {seconds}s")
 
-print(f"Samples {samples_count}, length {seconds}s")
+    t = np.linspace(0, seconds, samples_count, False)
+    soundwave = data_float * normalize_factor
+    # You can change int16 to uint16 and the audio result is different
+    # but the way it plays seems to be the same.
+    audio = soundwave.astype(np.int16)
 
-t = np.linspace(0, seconds, int(seconds * FREQUENCY), False)
-soundwave = data_float * np.iinfo(np.int16).max
-audio = soundwave.astype(np.int16)
+    play_obj = sa.play_buffer(audio, 1, sample_size, frequency)
+    play_obj.wait_done()
 
-play_obj = sa.play_buffer(audio, 1, BYTES_PER_SAMPLE, FREQUENCY)
-play_obj.wait_done()
+if __name__ == "__main__":
+    play_pcm(INPUT_FILE, FREQUENCY, BYTES_PER_SAMPLE)
