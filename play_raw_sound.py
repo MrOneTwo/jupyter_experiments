cdaf043eae176c09053a696d9d664180ce37e6d7
Michal Ciesielski <ciesielskimm@gmail.com>
2024-08-26 13:52:09 +0200

    Improve play_raw_sound.py

--- play_raw_sound.py
+++ play_raw_sound.py
@@ -3,14 +3,28 @@ import numpy as np
 import simpleaudio as sa
 from pathlib import Path
 from config import *
+import sys
 
-INPUT_FILE = SAMPLES_FILE
+
+FREQUENCY = 16000
+BYTES_PER_SAMPLE = 2
 
 
-def play_pcm(pcm_file: Path, frequency: int, sample_size: int):
-    data_unpacked = np.asarray(
-        [d[0] for d in struct.iter_unpack("<h", pcm_file.read_bytes())]
-    )
+def play_pcm(pcm_file: str, frequency: int, sample_size: int):
+    _data = Path(pcm_file).read_bytes()
+
+    bytes_per_sample = BYTES_PER_SAMPLE
+
+    if bytes_per_sample == 1:
+        normalize_factor = np.iinfo(np.uint8).max
+        data_unpacked = np.asarray(struct.unpack(f"<{len(_data)}b", _data))
+    elif bytes_per_sample == 2:
+        normalize_factor = np.iinfo(np.int16).max
+        data_unpacked = np.asarray(struct.unpack(f"<{len(_data)//2}h", _data))
+    elif bytes_per_sample == 4:
+        normalize_factor = np.iinfo(np.int32).max
+        data_unpacked = np.asarray(struct.unpack(f"<{len(_data)//4}i", _data))
+
     samples_count = len(data_unpacked)
 
     normalize_factor = np.iinfo(np.int16).max
@@ -33,4 +47,4 @@ def play_pcm(pcm_file: Path, frequency: int, sample_size: int):
     play_obj.wait_done()
 
 if __name__ == "__main__":
-    play_pcm(INPUT_FILE, FREQUENCY, BYTES_PER_SAMPLE)
+    play_pcm(sys.argv[1], FREQUENCY, BYTES_PER_SAMPLE)
